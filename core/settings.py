f8b5fb13ca296b4ff2f3959f78bf1a7732ba9444
davlatbekzoirov <davlatbekzoirov@gmail.com>
2026-06-11 22:10:54 +0500

    Dynamically load Celery Redis URL from environment variables

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = 'django-insecure-change-this-in-production-use-env-var'
-DEBUG = True
+DEBUG = DEBUG = env('DEBUG', default=False)
 ALLOWED_HOSTS = ['127.0.0.1','localhost','.onrender.com']
 
 INSTALLED_APPS = [
