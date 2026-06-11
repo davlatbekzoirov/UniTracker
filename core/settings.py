40707c3fe1b75a96e092f999d96600ea41bc7813
davlatbekzoirov <davlatbekzoirov@gmail.com>
2026-06-11 22:36:58 +0500

    Directly route Celery to live Render Redis URL

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,8 @@ CACHES = {
     }
 }
 
-CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='redis://127.0.0.1:6379/0')
-CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_BROKER_URL = "redis://red-d8lekdq8qa3s73ereei0:6379"
+CELERY_RESULT_BACKEND = "redis://red-d8lekdq8qa3s73ereei0:6379"
 CELERY_TIMEZONE = TIME_ZONE        
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
