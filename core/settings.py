024c53e39c08e5c5948ef3a0e27ad06fb2b9f650
davlatbekzoirov <davlatbekzoirov@gmail.com>
2026-06-13 05:03:35 +0500

    add: logo to base.html and edit: settings

--- core/settings.py
+++ core/settings.py
@@ -120,6 +120,7 @@ STORAGES = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [ BASE_DIR / 'static', ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
